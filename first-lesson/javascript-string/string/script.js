ca083b3e3471a696db4a4a1acf0758f1aa76ec54
abdillahinoor <abdillahinoor5@gmail.com>
2025-08-09 21:48:31 +0300

    i have created new files i want to push it to my github account

---
+++ first-lesson/javascript-string/string/script.js
@@ -0,0 +1,16 @@
+//different ways to create strings
+
+
+let firstName = "Abdillahi"; //using double quotes
+lastName = 'Noor'; //using single quotes
+let age = 30;       
+let isMarried = true;
+let city = new String("Mombasa"); //using string constructor
+let country = 'KENYA';       //using template literals back ticks
+// console.log(firstName,lastName,city.toString(),country);
+
+
+// how to access strings
+
+console.log(firstName.indexOf("A"));
+console.log(firstName.indexOf("b"));
